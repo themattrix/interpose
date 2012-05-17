4148a7af149379a847e54a6eb03acf1549791593
Matthew Tardiff <m.tardiff@astronautics.com>
2012-05-17 18:00:03 -0500

    Removed (yet) unnecessary type conversion maps. Added write() and build() functions to the interpose() class.

--- src/interpose.py
+++ src/interpose.py
@@ -1,97 +1,128 @@
 #!/usr/bin/python
 
+import subprocess
 from textwrap import dedent
 
-TYPE_CONVERSION={
-   'size_t' : 'unsigned long',
-   'int8_t' : 'char',
-   'uint8_t' : 'unsigned char',
-   'int16_t' : 'short',
-   'uint16_t' : 'unsigned short',
-   'int32_t' : 'long',
-   'uint32_t' : 'unsigned long',
-   'int64_t' : 'long long',
-   'uint64_t' : 'unsigned long long',
-   'signed char' : 'char',
-   'unsigned int' : 'unsigned',
-   'unsigned long int' : 'unsigned long',
-   'unsigned long long int' : 'unsigned long long',
-   'long int' : 'long',
-   'long long int' : 'long long'}
-
 class interpose(object):
-   def __init__(self, api):
+   def __init__(self, header, api):
+      self.header = header
+      self.out_lib = 'libinterpose_{0}.so'.format(self.header)
+      self.out_header = 'interpose_{0}'.format(self.header)
       self.api = api
+      self.code = ''
+      self.wrote = False
    def generate(self):
-      INCLUDES=(
-         'stdio.h',
-         'dlfcn.h',
-         'sys/time.h')
-      PRINTF_TYPES={
-         'char' : "'%c'",
-         'unsigned char' : '%u',
-         'short' : '',
-         'unsigned short' : '%u',
-         'int' : '%d',
-         'unsigned' : '%u',
-         'long' : '%ld',
-         'unsigned long' : '%lu',
-         'long long' : '%lld',
-         'unsigned long long' : '%llu'}
-      result = '\n'.join('#include <' + i + '>' for i in INCLUDES)
-      for function in self.api:
-         name = function[0]
-         args = function[1]
-         retn = function[2]
-         if retn == 'void':
-            save_result = ''
-            retn_result = ''
-         else:
-            save_result = "{0} result = "
-            retn_result = '''
-            
-            // return the actual result
-            return result;'''
-         result += dedent((
-         '''
+      if not self.code:
+         INCLUDES=(
+            '<stdio.h>',
+            '<dlfcn.h>',
+            '<sys/time.h>',
+            '"{0}"'.format(self.header))
+         result = '\n'.join('#include {0}'.format(i) for i in INCLUDES) + dedent('''
 
-         {0} {1}({2})
-         {{
-            // store the precise time-stamp for outputting later
+         struct timeval current_time()
+         {
+            // store a precise time-stamp
             struct timeval time;
 
             // capture the current time
             gettimeofday(&time, NULL);
+            
+            // return the time
+            return time;
+         }
+         ''')
+         for function in self.api:
+            name = function[0]
+            args = function[1]
+            retn = function[2]
+            if retn == 'void':
+               save_result = ''
+               retn_result = ''
+               dflt_result = 'return;'
+            else:
+               dflt = function[3]
+               save_result = "{0} result = ".format(retn)
+               retn_result = '''
+               
+               // return the actual result
+               return result;'''
+               dflt_result = "return {0};".format(dflt)
+            result += dedent((
+            '''
+            {0} {1}({2})
+            {{
+               // points to the original function being interposed
+               static {0} (*real_{1})({3}) = NULL;
 
-            // points to the original function being interposed
-            static {0} (*real_{1})({3}) = NULL;
+               // record the timestamp before the call
+               struct timeval call_timestamp = current_time();
 
-            // on the first call to this function, determine the pointer to the original function
-            if(!real_{1}) real_{1} = dlsym(RTLD_NEXT, "{1}");
+               // print the time-stamp
+               printf
+               (
+                  "[%011llu.%06llu][call] {1}()\\n",
+                  (long long unsigned)call_timestamp.tv_sec,
+                  (long long unsigned)call_timestamp.tv_usec
+               );
 
-            // run the original function
-            ''' + save_result + '''real_{1}({4});
+               // find the original function
+               if(!real_{1})
+               {{
+                  real_{1} = dlsym(RTLD_NEXT, "{1}");
 
-            // output the precise time-stamp and the function name
-            printf
-            (
-               "[%llu.%06llu] {1}()",
-               (long long unsigned)time.tv_sec,
-               (long long unsigned)time.tv_usec
-            );{5}
-         }}
-         ''').format(
-            retn,
-            name,
-            ', '.join('{0} {1}'.format(arg[1], arg[0]) for arg in args),
-            ', '.join(arg[1] for arg in args),
-            ', '.join(arg[0] for arg in args),
-            retn_result))
-      return result
+                  // if the original function is not found...
+                  if(!real_{1})
+                  {{
+                     // ...print an error...
+                     printf("   >>> ERROR: {1}() not found\\n");
+
+                     // ...and return immediately
+                     {7}
+                  }}
+               }}
+
+               // run the original function
+               {5}real_{1}({4});
+
+               // record the timestamp after the call
+               struct timeval done_timestamp = current_time();
+
+               // print the time-stamp
+               printf
+               (
+                  "   >>> duration: %llu.%06llu seconds\\n"
+                  "[%011llu.%06llu][done] {1}()\\n",
+                  (long long unsigned)(done_timestamp.tv_sec  - call_timestamp.tv_sec),
+                  (long long unsigned)(done_timestamp.tv_usec - call_timestamp.tv_usec),
+                  (long long unsigned)done_timestamp.tv_sec,
+                  (long long unsigned)done_timestamp.tv_usec
+               );{6}
+            }}
+            ''').format(
+               retn,
+               name,
+               ', '.join('{0} {1}'.format(arg[1], arg[0]) for arg in args),
+               ', '.join(arg[1] for arg in args),
+               ', '.join(arg[0] for arg in args),
+               save_result,
+               retn_result,
+               dflt_result))
+         self.code = result
+      return self.code
+   def write(self):
+      if not self.wrote:
+         with open(self.out_header, 'w') as f:
+            f.write(self.generate())
+         self.wrote = True
+   def build(self):
+      self.write()
+      subprocess.call(['gcc', '-shared', '-fPIC', '-Wall', '-Werror', '-std=c99', '-D_GNU_SOURCE', '-o', self.out_lib, self.out_header, '-ldl'])
 
 def main():
-   API=(('api_call', (('argc','int'),('argv', 'char *[]')), 'void'),)
-   print(interpose(API).generate())
+   API=('test_api.h', ('api_call', (('argc','int'),('argv', 'char **')), 'int', '-1'))
+   i = interpose(header = API[0], api = API[1:])
+   i.build()
 
 if __name__ == "__main__":
    main()
