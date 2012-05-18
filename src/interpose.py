b498fb36db2468e80e719a54d117a3d40a5b99c0
Matthew Tardiff <m.tardiff@astronautics.com>
2012-05-18 17:56:58 -0500

    Added a few docstrings to the Python code and some additional comments to the generated C code. The build() function of Interpose now prints the compilation command. Prettied-up the makefile output.

--- src/interpose.py
+++ src/interpose.py
@@ -16,12 +16,22 @@ else:
    CC_FLAGS     = ['-fPIC', '-Wall', '-Werror', '-std=c99', '-shared', '-D_GNU_SOURCE']
    CC_LIBS      = ['-ldl']
 
+def audit(message):
+   """Currently just a wrapper for print()."""
+   print(message)
+
 class Interpose(object):
+   """
+   Generate, write out, and compile C code for interposing API calls. The resulting library can be
+   used to intercept API calls for the specified function signatures if it is loaded before the
+   original library. On linux, this is achieved with the 'LD_PRELOAD=/path/to/lib.so' variable. On
+   OS X, use the 'DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=/path/to/lib.dylib' variables.
+   """
    def __init__(self, header, lib, api):
-      self.header = header
-      self.lib = lib
-      self.out_lib = 'libinterpose_{0}.{1}'.format(self.header, SO_EXTENSION)
-      self.out_body = 'interpose_{0}.c'.format(self.header)
+      self.real_header = header
+      self.real_lib = lib
+      self.interpose_lib = 'libinterpose_{0}.{1}'.format(self.real_header, SO_EXTENSION)
+      self.interpose_source = 'interpose_{0}.c'.format(self.real_header)
       self.api = api
       self.generated_code = ''
       self.wrote = False
@@ -31,7 +41,7 @@ class Interpose(object):
             '<stdio.h>',
             '<dlfcn.h>',
             '<sys/time.h>',
-            '"{0}"'.format(self.header))
+            '"{0}"'.format(self.real_header))
          result = '\n'.join('#include {0}'.format(i) for i in includes) + dedent('''
 
          struct timeval current_time()
@@ -82,12 +92,25 @@ class Interpose(object):
                if(!real_{1})
                {{
             #ifdef __APPLE__
+                  /** 
+                   ** On OS X, the original library is loaded explicitly and the function is
+                   ** queried from within that library. This technique does not work on linux; it
+                   ** results in an infinite recursion.
+                   **/
+
                   // grab handle to the original library
                   void *handle = dlopen("{8}", RTLD_NOW);
 
                   // find the original function within that library
                   real_{1} = ({0} (*)({3}))dlsym(handle, "{1}");
             #else
+                  /** 
+                   ** Retrieving a pointer to the original function is even easier in linux.
+                   ** Calling dlsym() with the flag "RTLD_NEXT" returns the *next* occurrence of
+                   ** the specified name, which is the original library call This does not work
+                   ** on OS X; it fails to find the function.
+                   **/
+
                   // find the original function
                   real_{1} = dlsym(RTLD_NEXT, "{1}");
             #endif
@@ -129,17 +152,19 @@ class Interpose(object):
                save_result,
                retn_result,
                dflt_result,
-               self.lib))
+               self.real_lib))
          self.generated_code = result
       return self.generated_code
    def write(self):
       if not self.wrote:
-         with open(self.out_body, 'w') as f:
+         with open(self.interpose_source, 'w') as f:
             f.write(self.generate())
          self.wrote = True
    def build(self):
       self.write()
-      subprocess.call([CC] + CC_FLAGS + ['-o', self.out_lib, self.out_body] + CC_LIBS)
+      cmd = [CC] + CC_FLAGS + ['-o', self.interpose_lib, self.interpose_source] + CC_LIBS
+      audit(">>> {0}".format(' '.join(c for c in cmd)))
+      subprocess.call(cmd)
 
 def main():
    API=('test_api.h', 'libtest_api.dylib', ('api_call', (('argc','int'),('argv', 'char **')), 'int', '-1'), ('api_simple', (), 'void'))
